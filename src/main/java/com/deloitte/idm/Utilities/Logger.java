8834ee76be89b0d7f36305aa107d48af0d962268
Doler <adoler@deloitte.com>
2017-06-23 12:47:58 -0300

    Configured connection

--- src/main/java/com/deloitte/idm/Utilities/Logger.java
+++ src/main/java/com/deloitte/idm/Utilities/Logger.java
@@ -8,8 +8,8 @@ import java.text.*;
 import java.util.*;
 
 public class Logger {
-    //protected static String defaultLogFile = "/opt/tomcat/webapps/ROOT/MsSQLConnectorLog.log";
-    protected static String defaultLogFile = "C:\\Users\\adoler\\Desktop\\Projects\\IAAS\\Configuration\\connectors\\Log\\MsSQLConnectorLog.log";
+    protected static String defaultLogFile = "/opt/tomcat/webapps/ROOT/MsSQLConnectorLog.log";
+
 
     public Logger() {}
 
