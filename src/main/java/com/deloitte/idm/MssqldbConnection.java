baa5cb3c2ef4d3477473baa0d7a00f41da73e2cc
Doler <adoler@deloitte.com>
2017-06-23 11:39:01 -0300

    Configured connection

---
+++ src/main/java/com/deloitte/idm/MssqldbConnection.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2010-2014 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.deloitte.idm;
+
+import com.deloitte.idm.Utilities.Logger;
+
+import javax.mail.Session;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MssqldbConnection {
+
+    private static final Logger LOG = new Logger();
+
+    private MssqldbConfiguration configuration;
+    private Connection connection = null;
+
+    public MssqldbConnection(MssqldbConfiguration configuration) throws SQLException {
+
+        this.configuration = configuration;
+        connect(configuration);
+    }
+
+    public Connection connect(final MssqldbConfiguration configuration) throws SQLException {
+        LOG.write("Attempting to initiate connection to database.");
+
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection= DriverManager.getConnection(
+                    "jdbc:sqlserver://"+configuration.getHost()+";databaseName="+configuration.getDbName()+"," +
+                            configuration.getUser()+","+configuration.getPassword());
+        }
+        catch (SQLException e) {
+            throw new SQLException("Failed to connect to database.", e.getMessage());
+        }
+        catch (Exception e) {
+            LOG.write(e.getMessage().toString());
+        }
+
+        return connection;
+    }
+
+    public void disconnect() {
+        try {
+            if (connection != null && connection.isValid(10));
+            connection.close();
+        }
+        catch (SQLException e) {
+            LOG.write(e.toString());
+        }
+        connection = null;
+        LOG.write("Disconnected from database.");
+    }
+
+    public Connection getInitializedConnection() throws SQLException {
+        if (connection == null || !connection.isValid(10)) {
+            connection = connect(configuration);
+        }
+
+        return connection;
+    }
+
+    public void testConnection() throws SQLException {
+        Connection connection = getInitializedConnection();
+        if (connection == null || !connection.isValid(10)) {
+            LOG.write("No existing connection. Attempting to reconnect...");
+            connection = connect(configuration);
+        }
+
+    }
+
+    public void dispose() {
+        //todo implement
+    }
+}
---
+++ src/main/java/com/deloitte/idm/Utilities/Logger.java
@@ -0,0 +1,48 @@
+package com.deloitte.idm.Utilities;
+
+/**
+ * Created by adoler on 23/6/2017.
+ */
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+public class Logger {
+    protected static String defaultLogFile = "/opt/tomcat/webapps/ROOT/MsSQLConnectorLog.log";
+
+    public Logger() {}
+
+    public static void write(String s) {
+        write(defaultLogFile, s);
+    }
+
+    public static void write(String f, String s) {
+        TimeZone tz = TimeZone.getTimeZone("EST"); // or PST, MID, etc ...
+        Date now = new Date();
+        DateFormat df = new SimpleDateFormat ("yyyy.mm.dd hh:mm:ss ");
+        df.setTimeZone(tz);
+        String currentTime = df.format(now);
+
+        FileWriter aWriter = null;
+        try {
+            aWriter = new FileWriter(f, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            aWriter.write(currentTime + " " + s + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            aWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            aWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
